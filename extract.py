93ed5127f400ff4d8cc224d461c16b451f7bc34e
Peter K <pkjar16@student.aau.dk>
2021-03-09 11:45:01 +0100

    Update extract.py

--- extract.py
+++ extract.py
@@ -2,60 +2,131 @@ import requests
 from bs4 import BeautifulSoup
 import cv2
 import urllib.request
-import os 
+import os, shutil
 
-URL = 'http://dai.cs.rutgers.edu/dai/s/occurrence?id_SignBankVariant=380'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+def run_hand_sign(variant_id):
+    URL = 'http://dai.cs.rutgers.edu/dai/s/occurrence?id_SignBankVariant=' + str(variant_id)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    rs_label = soup.select('center b')[0].get_text()
+    hand_sign_label = rs_label
+    hand_sign_label = hand_sign_label[31:]
 
-counter = 3
-globals()['id_list'] = []
+    if "\"" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("\"", "_")
 
-def hasNumbers(inputString):
-    try:
-        return any(char.isdigit() for char in inputString)
-    except:
-        return False
+    if "-" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("-", "_")
 
-def scrape_ids(soup, counter):
-    try:
-        result = soup.select('tr:nth-of-type(3) tr:nth-of-type('+str(counter)+') td:nth-of-type(8)')[0].get_text()
+    if "<" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("<", "_")
 
-        if hasNumbers(result) :
-            id_list.append(result)
-            scrape_ids(soup, counter+1)
+    if ">" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace(">", "_")
 
-    except:
-        return
+    if ":" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace(":", "_")
 
-scrape_ids(soup, counter)
-print(id_list)
-somelist = []
-for r in id_list:
-    res = ''.join(filter(lambda i: i.isdigit(), r))
-    somelist.append(res)
-print(somelist)
+    if "/" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("/", "_")
 
-for video in somelist:
-    vid_url = 'http://dai.cs.rutgers.edu/dai/s/video?type=separate&id=' + str(video)
-    pg = requests.get(vid_url)
-    sp = BeautifulSoup(pg.content, 'html.parser')
-    tags = sp.find_all('video')
-    children = tags[0].findChildren("source" , recursive=False)
-    ulz = str(children[0])
-    ulz = ulz[:-3]
-    ulz = ulz[13:]
-    print(ulz)
-    urllib.request.urlretrieve(ulz, 'videos\\' + str(video))
-    vidcap = cv2.VideoCapture('videos\\' + str(video))
-    success,image = vidcap.read()
-    count = 0
-    directory = "images\\" + str(video)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if r"\\" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace(r"\\", "_")
 
-    while success:
-        cv2.imwrite("images\\"+ str(video) + "\\frame%d.jpg" % count, image)     # save frame as JPEG file      
+    if "|" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("|", "_")
+
+    if "?" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("?", "_")
+
+    if "*" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("*", "_")
+
+    if "@" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("@", "_")
+
+    if "+" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("+", "_")
+
+    if "." in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace(".", "_")
+
+    if "(" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("(", "_")
+
+    if ")" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace(")", "_")
+
+    if " " in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace(" ", "_")
+
+    if "#" in hand_sign_label:
+        hand_sign_label = hand_sign_label.replace("#", "_")
+
+    counter = 3
+    globals()['id_list'] = []
+
+    def hasNumbers(inputString):
+        try:
+            return any(char.isdigit() for char in inputString)
+        except:
+            return False
+
+    def scrape_ids(soup, counter):
+        try:
+            result = soup.select('tr:nth-of-type(3) tr:nth-of-type('+str(counter)+') td:nth-of-type(8)')[0].get_text()
+
+            if hasNumbers(result) :
+                id_list.append(result)
+                scrape_ids(soup, counter+1)
+
+        except:
+            return
+
+    scrape_ids(soup, counter)
+    print(id_list)
+    somelist = []
+    for r in id_list:
+        res = ''.join(filter(lambda i: i.isdigit(), r))
+        somelist.append(res)
+    print(somelist)
+
+    for video in somelist:
+        vid_url = 'http://dai.cs.rutgers.edu/dai/s/video?type=separate&id=' + str(video)
+        pg = requests.get(vid_url)
+        sp = BeautifulSoup(pg.content, 'html.parser')
+        tags = sp.find_all('video')
+        children = tags[0].findChildren("source" , recursive=False)
+        ulz = str(children[0])
+        ulz = ulz[:-3]
+        ulz = ulz[13:]
+        print(ulz)
+        urllib.request.urlretrieve(ulz, 'videos\\' + str(video))
+        vidcap = cv2.VideoCapture('videos\\' + str(video))
         success,image = vidcap.read()
-        print('Read a new frame: ', success)
-        count += 1
+        count = 0
+        directory = "images\\" + hand_sign_label+ "\\" + str(video)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        while success:
+            cv2.imwrite(directory + "\\frame%d.jpg" % count, image)     # save frame as JPEG file      
+            success,image = vidcap.read()
+            print('Read a new frame: ', success)
+            count += 1
+
+def clear_vid_folder():
+    folder = 'videos'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+for i in range(6000):
+    run_hand_sign(i)
+    clear_vid_folder()
